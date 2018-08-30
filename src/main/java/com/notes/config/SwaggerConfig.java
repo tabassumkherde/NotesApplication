bedfb7a376a24582047d20cc46a4794ef2e4e5d9
tabassum <tabassumsha@gmail.com>
2018-08-30 10:06:11 +0530

    Added Basic HTTP Authentication

--- src/main/java/com/notes/config/SwaggerConfig.java
+++ src/main/java/com/notes/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@EnableWebSecurity
 @ComponentScan("com.notes.controller.*")
 public class SwaggerConfig {
 	/**
